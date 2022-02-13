7cb044685cad07ca925b68b2e5e7c31c2b806b53
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-02-13 10:13:29 -0600

    sunday updates

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 'use strict'
 
-let input_Title= document.getElementById("input_Title")
-let input_Rating= document.getElementById("input_Rating")
-let input_Year= document.getElementById("input_Year")
-let input_Director= document.getElementById("input_Director")
-let input_Plot= document.getElementById("input_Plot")
-let input_Genre= document.getElementById("input_Genre")
-let input_Actors= document.getElementById("input_Actors")
+let input_Title = document.getElementById("input_Title")
+let input_Rating = document.getElementById("input_Rating")
+let input_Year = document.getElementById("input_Year")
+let input_Director = document.getElementById("input_Director")
+let input_Plot = document.getElementById("input_Plot")
+let input_Genre = document.getElementById("input_Genre")
+let input_Actors = document.getElementById("input_Actors")
 
 //initial load better idea to load until main is done, loader will be inside main fetch
 // $(window).on("load",()=>{setTimeout(()=>$("#loading_Screen").fadeOut("slow"),1000)})
 //calls to load and unload our screen -- tried to make screen variable, but fadein/out doesnt like that.
-let loading= ()=>$("#loading_Screen").fadeIn("fast")
-let finished= ()=>$("#loading_Screen").fadeOut("slow")
+let loading = () => $("#loading_Screen").fadeIn("fast")
+let finished = () => $("#loading_Screen").fadeOut("slow")
 
 let url = 'https://mma-movies.glitch.me/movies'
 let html = document.getElementById("movie__cards")
+
 function getMovie() {
- loading();
-    html.innerHTML= ""
+    loading();
+    html.innerHTML = ""
     $.ajax('https://mma-movies.glitch.me/movies')
         .done((data) => {
             console.log(data)
@@ -27,40 +28,47 @@ function getMovie() {
 
                 //console.log(moviesArray);
                 html.innerHTML +=
-
-                    "<div class='card col-3 border border-primary'>" +
-                    "<img class='card-img-top img-thumbnail' src='" + movie.poster + "' alt='" + movie.title +
-                    "'<div class='card-body'><h5 class='card-title'>" + movie.title + "</h5><p class='card-text'>" +
-                    movie.plot + "</p>" +
-                    "<ul class='list-group list-group-flush'><li class='list-group-item'>" + movie.director +
-                    ", year:" + movie.year + "</li><li class='list-group-item'>" + movie.actors +
-                    "</li><li class='list-group-item'>" + movie.genre + ", rating: " + movie.rating +
-                    "</li></ul><div class='card-body'><a href='#' class='card-link'><button data-id='" + movie.id +
-                    "' class='edit float-left' onclick='edit($(this).attr(\"data-id\"))'>edit</button> <button data-id='" + movie.id +
-                    "' class='delete float-right' onclick='del($(this).attr(\"data-id\"))'>delete</button></a></div></div></div>"
+// language = html
+                    `<div class='card col-3 m-1'>
+                    <img class='card-img-top img-thumbnail' src='${movie.poster}' alt='${movie.title}'>
+                        <div class='card-body'>
+                        <h5 class='card-title text-center'>${movie.title}(${movie.year})</h5>
+                        <p class='card-text'>${movie.plot}</p>
+                        <ul class='list-group list-group-flush text-center'>
+                            <li class='list-group-item'>Director:<br> ${movie.director}</li>
+                            <li class='list-group-item'>Actors:<br> ${movie.actors}</li>
+                            <li class='list-group-item'>Genre(s): ${movie.genre}</li>
+                            <li class='list-group-item'>Rating:<br> ${movie.rating}</li>
+                        </ul>
+                            <div class='change d-flex justify-content-between p-0'>
+                                <img src='assets/button2.png' alt='Edit' data-id='${movie.id}' class='edit align-self-end' onclick='edit($(this).attr(\"data-id\"))' height='40px' width='60px'>
+                                <img src='assets/button.png' alt='Delete' data-id='${movie.id}' class='delete align-self-end' onclick='del($(this).attr(\"data-id\"))' height='40px' width='60px'>
+                            </div>
+                        </div>
+                    </div>`
 
             })
-        setTimeout(()=>finished(),1000);
+            setTimeout(() => finished(), 1000);
         })
 }
-getMovie()
 
+getMovie()
 
 
 //ADDING
 // creat button  html
 //grab btn jquery click ()  eventregister : fetch ( ).post :  form
 
-function add (){
+function add() {
     let addMovie = {
-            title: input_Title.value,
-            rating: input_Rating.value,
-            year: input_Year.value,
-            director: input_Director.value,
-            plot: input_Plot.value,
-            genre: input_Genre.value,
-            actors: input_Actors.value,
-            poster: "none"
+        title: input_Title.value,
+        rating: input_Rating.value,
+        year: input_Year.value,
+        director: input_Director.value,
+        plot: input_Plot.value,
+        genre: input_Genre.value,
+        actors: input_Actors.value,
+        poster: "none"
     }
     $.post(url, addMovie).done(getMovie)
     $("#form").toggleClass("none active")
@@ -68,10 +76,10 @@ function add (){
 
 document.getElementById("add_btn").addEventListener("click", add); // submit button add function
 
-document.getElementById("open_form_add").addEventListener("click",()=>{
+document.getElementById("open_form_add").addEventListener("click", () => {
 //these will make sure its opened on click. not just toggled
-$("#form").toggleClass("none active");
-$("#add_btn").toggleClass("none active");
+    $("#form").toggleClass("none active");
+    $("#add_btn").toggleClass("none active");
 })
 
 //DELETING
@@ -82,22 +90,22 @@ function del(id) {
     let delMovie = {
         method: 'DELETE',
     }
-     fetch(url+`/${id}`, delMovie).then(getMovie)
+    fetch(url + ` /${id}`, delMovie).then(getMovie)
 }
 
 // EDITING : form
 // when it click edit button, it will creat a form for editing   and inside the adding form (?)  we will have submit button that will actually go to server - edit post.
 // turns out for editing Headers are important. at leaast in our case
-function edit(id){
-let edit =$("#Edit_btn")
+function edit(id) {
+    let edit = $("#Edit_btn")
     $("#form").toggleClass("none active");
     edit.toggleClass("none active");
 
-        edit.on('click',()=> {
+    edit.on('click', () => {
 
-            fetch(url+`/${id}`,{
+        fetch(url + ` /${id}`, {
             method: "PUT",
-            headers: { 'Content-Type': 'application/json' },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 title: input_Title.value,
                 rating: input_Rating.value,
@@ -107,19 +115,22 @@ let edit =$("#Edit_btn")
                 genre: input_Genre.value,
                 actors: input_Actors.value,
                 poster: "none",
-                id:id
+                id: id
             }),
-        }).then((response)=>console.log(response.json()))
-            .then(()=>{getMovie();});
+        }).then((response) => console.log(response.json()))
+            .then(() => {
+                getMovie();
+            });
     })
 }
 
-$("#close").click(()=>{
-    function removeOptions(input){
-        if(input.hasClass("active")){
+$("#close").click(() => {
+    function removeOptions(input) {
+        if (input.hasClass("active")) {
             input.toggleClass("none active")
         }
     }
+
     removeOptions($("#add_btn"));
     removeOptions($("#Edit_btn"));
     $("#form").toggleClass("none active");
